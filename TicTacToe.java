35399afbf899e0b8604bfb62625be93bf3a7c340
guna-1527 <gunaprasanth1527@gmail.com>
2024-07-30 23:00:13 +0530

    tic tac toe game

---
+++ TicTacToe.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    static char[][] board;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        initBoard();
+        ;
+    }
+
+    static void displayBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i <= 2; i++) {
+            System.out.print("| ");
+            for (int j = 0; j <= 2; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+        System.out.println();
+    }
+
+    static void initBoard() {
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    static void placeMark(int row, int col, char mark) {
+        board[row][col] = mark;
+    }
+
+    static boolean verticalWin() {
+        for (int j = 0; j <= 2; j++) {
+            if (board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][0] == board[2][j]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean horizontalWin() {
+        for (int j = 0; j <= 2; j++) {
+            if (board[j][0] != ' ' && board[j][0] == board[j][1] && board[j][1] == board[j][2]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean DiagonalWin() {
+        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]
+                || board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TicTacToe t = new TicTacToe();
+        HumanPlayer p1 = new HumanPlayer("guna", 'X');
+        HumanPlayer p2 = new HumanPlayer("kamal", 'O');
+        HumanPlayer currentPlayer;
+        currentPlayer = p1;
+
+        while (true) {
+            System.out.println(currentPlayer.name + " turn: ");
+            currentPlayer.makeMove();
+            TicTacToe.displayBoard();
+
+            if (TicTacToe.DiagonalWin() || TicTacToe.verticalWin() || TicTacToe.horizontalWin()) {
+                System.out.println(currentPlayer.name + "  won the match");
+                break;
+            } else {
+                if (currentPlayer == p1) {
+                    currentPlayer = p2;
+                } else {
+                    currentPlayer = p1;
+                }
+            }
+        }
+    }
+}
+
+class HumanPlayer {
+    String name;
+    char mark;
+
+    HumanPlayer(String name, char mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    void makeMove() {
+        int row;
+        int col;
+        do {
+            System.out.println("Enter the row and col: ");
+            Scanner scan = new Scanner(System.in);
+            row = scan.nextInt();
+            col = scan.nextInt();
+        } while (!isValidMove(row, col));
+
+        TicTacToe.placeMark(row, col, mark);
+    }
+
+    boolean isValidMove(int row, int col) {
+        if (row <= 2 && row >= 0 && col <= 2 && col >= 0) {
+            if (TicTacToe.board[row][col] == ' ') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
